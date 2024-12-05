1e92e63c62e0df013f6dd8e74f63367ad01ae949
jhordycamacas <jhordycamacaas9@gmail.com>
2024-12-05 14:33:27 -0500

    ejer3

--- Taller08/src/problemas/Problema03.java
+++ Taller08/src/problemas/Problema03.java
@@ -14,7 +14,21 @@ public class Problema03 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+             // TODO code application logic here
+        int limite = 5;
+        String impresion = "";
+        for (int n = 1; n <= limite; n++) {
+            for (int i = 1; i <= n; i++) {
+                impresion = impresion + "*";
+            }
+            impresion = impresion + "\n";
+        }
+        for (int n = 5 ; n >= 1; n--) {
+            for (int i = 1; i<= n; i++) {
+                impresion = impresion + "*";
+            }
+            impresion = impresion + "\n";
+        }
+        System.out.printf("%s", impresion);
     }
-    
 }
